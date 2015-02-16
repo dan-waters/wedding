7a9ae943597a09085cbddb8078a83cafb9cca118
Daniel Waters <daniel.waters@softwire.com>
2015-02-16 12:58:54 +0000

    Adding simple nav

---
+++ config/navigation.rb
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Configures your navigation
+SimpleNavigation::Configuration.run do |navigation|
+  navigation.renderer = SimpleNavigationRenderers::Bootstrap3
+  # Specify the class that will be applied to active navigation items.
+  # Defaults to 'selected' navigation.selected_class = 'your_selected_class'
+
+  # Specify the class that will be applied to the current leaf of
+  # active navigation items. Defaults to 'simple-navigation-active-leaf'
+  # navigation.active_leaf_class = 'your_active_leaf_class'
+
+  # Define the primary navigation
+  navigation.items do |primary|
+    # Add an item to the primary navigation. The following params apply:
+    # key - a symbol which uniquely defines your navigation item in the scope of the primary_navigation
+    # name - will be displayed in the rendered navigation. This can also be a call to your I18n-framework.
+    # url - the address that the generated item links to. You can also use url_helpers (named routes, restful routes helper, url_for etc.)
+    # options - can be used to specify attributes that will be included in the rendered navigation item (e.g. id, class etc.)
+    #           some special options that can be set:
+    #           :if - Specifies a proc to call to determine if the item should
+    #                 be rendered (e.g. <tt>if: -> { current_user.admin? }</tt>). The
+    #                 proc should evaluate to a true or false value and is evaluated in the context of the view.
+    #           :unless - Specifies a proc to call to determine if the item should not
+    #                     be rendered (e.g. <tt>unless: -> { current_user.admin? }</tt>). The
+    #                     proc should evaluate to a true or false value and is evaluated in the context of the view.
+    #           :method - Specifies the http-method for the generated link - default is :get.
+    #           :highlights_on - if autohighlighting is turned off and/or you want to explicitly specify
+    #                            when the item should be highlighted, you can set a regexp which is matched
+    #                            against the current URI.  You may also use a proc, or the symbol <tt>:subpath</tt>.
+    #
+    primary.item :people, 'People', people_path
+
+    primary.item :items, 'Items', items_path
+  end
+end
