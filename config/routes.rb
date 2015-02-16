780ca5bcb351cae446a33f44fe734d58441c57fe
Daniel Waters <daniel.waters@softwire.com>
2015-02-16 12:12:45 +0000

    Adding home controller with links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :items do
     resources :payments
   end
 
   resources :people
 
-  root 'items#index'
+  root 'home#index'
 end
