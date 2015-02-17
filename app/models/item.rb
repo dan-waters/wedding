a2f37e02b66d744ce1fb44d6f876d90070c98f3c
Daniel Waters <daniel.waters@softwire.com>
2015-02-17 20:04:09 +0000

    Adding information to the homepage

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ActiveRecord::Base
   has_many :payments, dependent: :destroy
   has_many :people, through: :payments
+  scope :booked, -> { where(booked: true) }
+  scope :not_booked, -> { where(booked: false) }
 
   def self.find_by_param(param)
     find_by!(name: param)
