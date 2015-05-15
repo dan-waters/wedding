61f4329d2fa71127164ccccc170212ce592b0700
Daniel Waters <daniel.waters@softwire.com>
2015-05-15 21:13:33 +0100

    adding destination validation

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,28 @@
 class Destination < ActiveRecord::Base
   belongs_to :honeymoon
   validates_presence_of :name, :arrival_date, :departure_date
+  validate :dates_do_not_overlap, :dates_inside_honeymoon_range
+
+  default_scope { order(:arrival_date) }
+
+  private
+
+  def dates_do_not_overlap
+    if Destination.any?{|dest| dest != self && dest.arrival_date < arrival_date && dest.departure_date > arrival_date}
+      errors.add(:arrival_date, 'must not overlap with another destination')
+    end
+    if Destination.any?{|dest| dest != self && dest.arrival_date < departure_date && dest.departure_date > departure_date}
+      errors.add(:departure_date, 'must not overlap with another destination')
+    end
+  end
+
+  def dates_inside_honeymoon_range
+    if arrival_date < Honeymoon.instance.from
+      errors.add(:arrival_date, 'must be after honeymoon has begun')
+    end
+
+    if departure_date > Honeymoon.instance.to
+      errors.add(:departure_date, 'must be before honeymoon has ended')
+    end
+  end
 end
